011cf38d4f15b049b34eb0280e70d27e52c290ac
krlim417 <krlim417@gmail.com>
2017-11-04 02:31:07 -0700

    Move form items to the AddGrocery file, updates state on input changes, and sends state to app's state update function

--- client/src/components/AddGrocery.jsx
+++ client/src/components/AddGrocery.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import index from '../index.jsx';
 
 class AddGrocery extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
+      quantity: null,
+      description: ''
+    }
+    this.onInputChangeUpdateState = this.onInputChangeUpdateState.bind(this);
+    this.onAddGroceryClickAddItem = this.onAddGroceryClickAddItem.bind(this);
+  }
+
+  onAddGroceryClickAddItem(e) {
+    e.preventDefault();
+    this.props.func(this.state);
+  }
+
+  onInputChangeUpdateState(e) {
+    if (e.target.id === 'quantity') {
+      this.state[e.target.id] = parseInt(e.target.value);
+    } else {
+      this.state[e.target.id] = e.target.value;
     }
   }
 
   render () {
-    return null;
+    return (
+      <div>
+        <form>
+          Description: <input type="text" id="description" onChange={this.onInputChangeUpdateState} /><br />
+          Quantity: <input type="text" id="quantity" onChange={this.onInputChangeUpdateState} />
+          <input type="submit" value="Add Grocery" onClick={this.onAddGroceryClickAddItem} />
+        </form>
+      </div>
+    )
   }
 }
 
